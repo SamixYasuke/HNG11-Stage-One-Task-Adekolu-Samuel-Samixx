4c22ff64517a87665f515e0791610001e95f6b8c
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-07-01 01:39:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import express from "express";
 import dotenv from "dotenv";
+import {
+  getLocationData,
+  getWeatherData,
+} from "./utilities/weatherLocationUtils.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/api/hello/", async (req, res) => {
+app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
-
-  if (!visitor_name) {
-    return res.status(400).json({ error: "Visitor name is required" });
-  }
-
   try {
-    // This is the i got the user's public IP address
+    if (!visitor_name) {
+      return res.status(400).json({ error: "Visitor name is required" });
+    }
+
     const ipAddress =
       req.headers["x-real-ip"] ||
       req.headers["x-forwarded-for"] ||
@@ -23,21 +25,14 @@ app.get("/api/hello/", async (req, res) => {
       "";
     console.log(ipAddress);
 
-    // This is where i fetched the location data and city name based on IP address
-    const locationUrl = `https://ipapi.co/${ipAddress}/json/`;
-    const locationResponse = await axios.get(locationUrl);
-    const city = locationResponse?.data?.city;
+    const city = await getLocationDataFromIp(ipAddress);
+    const weatherInfo = await getWeatherData(city);
 
-    // This is where i fetched the weather data based on city name
     if (city) {
-      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process?.env?.OPENWEATHER_API_KEY}&units=metric`;
-      const weatherResponse = await axios.get(weatherUrl);
-      const weatherInfo = weatherResponse?.data?.main?.temp;
-
-      res.json({
+      res.status(200).json({
         client_ip: ipAddress,
         location: city,
-        greeting: `Hello, ${visitor_name}!, the temperature is ${weatherInfo} degrees Celsius in ${city}`,
+        greeting: `Hello, ${visitor_name}!, the temperature is ${weatherInfo} degrees Celcius in ${city}`,
       });
     } else {
       throw new Error("City name not found in location data");
